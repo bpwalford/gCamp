15a5da443027f9884116fa2264be857acd98a4bf
bpwalford <bpwalford@gmail.com>
2014-10-09 14:09:49 -0600

    added haml version of splash page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "pages#index"
 
+  get 'pages/2' => 'pages#index2'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
