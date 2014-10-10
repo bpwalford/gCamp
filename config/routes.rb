06eb7b0108bf47e2a3f992971496fa7eb32b640a
bpwalford <bpwalford@gmail.com>
2014-10-10 09:01:10 -0600

    added splash version with background image

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'columns' => 'pages#index3'
 
+  get 'backimage' => 'pages#index4'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
