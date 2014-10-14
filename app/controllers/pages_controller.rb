b0a5b0c44fe590b80e43737b2f54709a0dfb75d8
bpwalford <bpwalford@gmail.com>
2014-10-14 13:52:41 -0600

    took out src tags and added images to assets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,19 +5,19 @@ class PagesController < ApplicationController
     @panel_elements = [
 
       {
-        image: 'https://farm3.staticflickr.com/2833/8865586006_c5ce2a79e6_n.jpg',
+        image: 'tasksImage.jpg',
         title: 'Tasks',
         content: ['Grouped by projects and lists. Just the way you like \'em'],
       },
 
       {
-        image: 'https://farm4.staticflickr.com/3832/8865584540_b5307795be_n.jpg',
+        image: 'documentsImage.jpg',
         title: 'Documents',
         content: ['Upload', 'Comment', 'Revise']
       },
 
       {
-        image: 'https://farm4.staticflickr.com/3821/9232183203_859067da50_n.jpg',
+        image: 'commentsImage.jpg',
         title: 'Comments',
         content: ['Comment on tasks and documents', 'Get email notifications']
       }
