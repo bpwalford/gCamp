f2f50321e1b3267e1748a453134b67ced3016496
bpwalford <bpwalford@gmail.com>
2014-10-20 14:52:16 -0600

    added complete column to tasks table:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016240034) do
+ActiveRecord::Schema.define(version: 20141020204807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20141016240034) do
     t.string   "desription"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "complete"
   end
 
 end
