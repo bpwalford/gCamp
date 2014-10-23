52064e5600a9c4c38e44642bd0eb97b453b01829
bpwalford <bpwalford@gmail.com>
2014-10-23 15:20:53 -0600

    added sort links to tasks index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,13 @@ class TasksController < ApplicationController
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = Task.all
+
+    if params[:sort] == "all"
+      @tasks = Task.all
+    else
+      @tasks = Task.where(complete: nil)
+    end
+
   end
 
   # GET /tasks/1
