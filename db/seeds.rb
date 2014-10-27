b36d8d4adeb0d2f16f59584060b64566f4cfd85c
bpwalford <bpwalford@gmail.com>
2014-10-27 13:53:57 -0600

    added users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Task.delete_all
+User.delete_all
 
 20.times do
 
   Task.create description: Faker::Lorem.sentence(3),
               due_date: Faker::Date.forward(30),
               complete: [true, false].sample
+
+end
+
+14.times do
+
+  User.create first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email
 
 end
