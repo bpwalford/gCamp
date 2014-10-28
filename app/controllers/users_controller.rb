b715746d8eeef3515f8db2a1a931348cd11e2dc3
bpwalford <bpwalford@gmail.com>
2014-10-28 08:23:22 -0600

    added update function to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update]
+
   def index
     @users = User.all
   end
@@ -8,8 +10,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+
   def create
-    @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
+    @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
@@ -18,6 +23,26 @@ class UsersController < ApplicationController
         format.html {render :new}
       end
     end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email)
   end
 
 end
