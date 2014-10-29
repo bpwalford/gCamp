268b0e8952427ad7f7e5bd7a0ab8418362491258
bpwalford <bpwalford@gmail.com>
2014-10-29 10:57:22 -0600

    complete button now works and retains params

--- Gemfile
+++ Gemfile
@@ -45,3 +45,5 @@ gem 'bootstrap-sass', '~> 3.2.0'
 gem 'autoprefixer-rails'
 
 gem 'faker'
+
+gem 'pry'
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -13,25 +13,25 @@ module TasksHelper
 
   def task_true_false(task, sort, table_sort)
 
-    if task.complete == false
-
-      link_to content_tag(:span, "", class: "glyphicon glyphicon-unchecked"),
-
-      toggleCompletion_path(currentId: task.id, currentSort: sort, currentTableSort: table_sort),
-
-      class: "btn btn-default btn-sm"
-
-    elsif task.complete == true
-
-      link_to content_tag(:span, "", class: "glyphicon glyphicon-check"),
-
-      toggleCompletion_path(currentId: task.id, sort: sort, tableSort: table_sort),
-
-      class: "btn btn-default btn-sm"
-
-    end
+    # if task.complete == false
+    #
+    #   link_to content_tag(:span, "", class: "glyphicon glyphicon-unchecked"),
+    #
+    #   toggleCompletion_path(currentId: task.id, currentSort: sort, currentTableSort: table_sort),
+    #
+    #   class: "btn btn-default btn-sm"
+    #
+    # elsif task.complete == true
+    #
+    #   link_to content_tag(:span, "", class: "glyphicon glyphicon-check"),
+    #
+    #   toggleCompletion_path(currentId: task.id, sort: sort, tableSort: table_sort),
+    #
+    #   class: "btn btn-default btn-sm"
+    #
+    # end
 
   end
-  
+
 
 end
