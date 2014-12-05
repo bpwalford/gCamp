9ba587cecd49837cd439c8b9b6ac06bbab5933fd
bpwalford <bpwalford@gmail.com>
2014-12-04 17:01:57 -0700

    started sessions controller spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < PublicController
 
   def create
+    # binding.pry
     user = User.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,55 @@
-require 'rails/helper'
+require 'rails_helper'
 
 describe SessionsController do
 
+  before do
+    @user = create_user(
+      email: 'asdf@asdf.com',
+      password: 'asdf'
+    )
+    @project = create_project
+    @membership = create_membership(
+      user: @user,
+      project: @project
+    )
+    @tcejorp = create_project
+  end
 
+  describe '#create' do
+
+    it 'redirects to signin path with invalid credetials' do
+      post :create, session: {email: 'asdf', password: 'asdf'}
+      expect(response).to redirect_to(signin_path(invalid: true))
+    end
+
+    it 'redirects to attempted destination upon login after attempt' do
+      post :create,
+        session: {
+          email: 'asdf@asdf.com',
+          password: 'asdf',
+          attempt: 'true',
+          place: "/project/#{@project.id}"
+        }
+      expect(response).to redirect_to("/project/#{@project.id}")
+      expect(session[:user_id]).to eq(@user.id)
+    end
+
+    it 'redirects to projects when attempted request was not accessible' do
+      post :create,
+        session: {
+          email: 'asdf@asdf.com',
+          password: 'asdf',
+          attempt: 'true',
+          place: "/project/#{@tcejorp.id}"
+        }
+      expect(response).to redirect_to(projects_path)
+      expect(session[:user_id]).to eq(@user.id)
+    end
+
+  end
+
+
+  describe '#destroy' do
+  end
 
 end
