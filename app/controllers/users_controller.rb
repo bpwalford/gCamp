122642eb7bff340969d946ad7d052ff5f08716dc
bpwalford <bpwalford@gmail.com>
2014-12-10 16:08:28 -0700

    user's can now add pivitol tracker tokens to profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,9 +54,9 @@ class UsersController < ApplicationController
 
   def user_params
     if admin?
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :admin)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :tracker_token, :admin)
     else
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :tracker_token)
     end
   end
 
