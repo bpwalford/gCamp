193497ff3e1b1dbc35f295c5b86e95b5e4c4a11e
bpwalford <bpwalford@gmail.com>
2014-12-22 08:59:28 -0700

    added ip's to users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
   def crumbs(links)
-
     content_tag(:ol, class: "breadcrumb") do
       links.each do |item|
         if item[:link] == "active"
@@ -11,7 +10,14 @@ module ApplicationHelper
         end
       end
     end
+  end
 
+  def page_header2(title, &block)
+    content_for(:title, title)
+    content_tag(:div, class: "page-header") do
+      concat(content_tag(:h1, title))
+      concat(content_tag(:div, content_tag(:span, yield), class: "pull-right"))
+    end
   end
 
 end
